bac4a10fa0707584c1b8bbb7e7ec51d5be6e74be
bredele <olivier.wietrich@gmail.com>
2017-04-08 17:25:11 -0600

    implement tri shaker

--- index.js
+++ index.js
@@ -8,17 +8,18 @@
  * @api public
  */
 
-module.exports = function(array) {
+module.exports = function(array, distance) {
+  distance = distance || ((prev, next) => prev < next)
   array = array.slice(0)
   var swapped = true
   var start = 0
-  var end = array.length
+  var end = array.length - 1
   while(swapped) {
     swapped = false
     var i = start
     while(i < end) {
       var tmp = array[i + 1]
-      if(array[i] > tmp) {
+      if(distance(tmp, array[i])) {
         array[i + 1] = array[i]
         array[i] = tmp
         swapped = true
@@ -28,7 +29,7 @@ module.exports = function(array) {
     var j = end--
     while(j > start) {
       var tmp = array[j - 1]
-      if(tmp > array[j]) {
+      if(distance(array[j], tmp)) {
         array[j - 1] = array[j]
         array[j] = tmp
         swapped = true
